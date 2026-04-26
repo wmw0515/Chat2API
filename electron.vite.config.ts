733265b0913bfd624e63bda12537f9afed0a5834
MadLab <70572132+wmw0515@users.noreply.github.com>
2026-04-27 00:11:54 +0800

    feat: add minimal headless proxy server mode

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -24,7 +24,8 @@ export default defineConfig({
     build: {
       rollupOptions: {
         input: {
-          index: resolve(__dirname, 'src/main/index.ts')
+          index: resolve(__dirname, 'src/main/index.ts'),
+          headless: resolve(__dirname, 'src/main/headless.ts')
         },
         output: {
           format: 'cjs'
